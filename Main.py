22cdc189278f62588440b643922fd4e206e49f06
Carolina <caro.villa40@gmail.com>
2019-07-22 23:35:41 -0300

    FuncionandoAndo version2

--- Main.py
+++ Main.py
@@ -3,10 +3,9 @@ from pygame.locals import *
 
 def main():
 
-    pantalla = pygame.display.set_mode((450,300),0,32)
+    pantalla = pygame.display.set_mode((400,600),0,32)
     pygame.display.set_caption("Modulo de fuentes")
 
-    reloj = pygame.time.Clock()
 
     fuente = pygame.font.Font(None, 30)
     texto1 = fuente.render("G", 0, (255, 255, 255))
@@ -15,21 +14,27 @@ def main():
     vel = 5
 
 
+
+
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-
-        keys = pygame.key.get_pressed()
-
-        y-=-1
+        reloj = pygame.time.Clock()
+       
+        y += vel
         pantalla.fill((0,0,0))
 
         reloj.tick(20)
 
         pantalla.blit(texto1, (x,y))
+
+        pygame.draw.line(pantalla, (40, 210, 250), (0, 500), (400, 500), 1)
         pygame.display.update()
+
+
 
 if __name__ == '__main__':
     pygame.init()
