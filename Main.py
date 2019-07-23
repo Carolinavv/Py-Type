ad35dd262cba88c060800e9cebc1c8d951f64356
Carolina Soledad Villa <CVilla@sofrecom.com.ar>
2019-07-23 11:21:31 -0300

    En proceso

--- Main.py
+++ Main.py
@@ -1,43 +1,88 @@
 import pygame
+import random
 from pygame.locals import *
+
+def dameLetraApretada(key):
+    dicc = {
+
+    }
 
 def main():
     pygame.init()
 
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                
+    key = pygame.key.get_pressed()
     pantalla = pygame.display.set_mode((400,600),0,32)
     pygame.display.set_caption("Prueba")
-
-
     fuente = pygame.font.Font(None, 30)
-    texto1 = fuente.render("G", 0, (255, 255, 255))
-    x = 50
-    y = 50
-    vel = 5
-    run = True
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    puntaje = fuente.render("Puntaje", 0, (255, 0, 0))
+    lista = ["casa","hola","mano","jamon"]
+    palabraUsuario = ""
+    puntos = 0
+   
+    for palabra in lista:
+        
+        texto = fuente.render(palabra, 0, (255, 255, 255))
+       
+
+        puntos = 0
+        
+        x = (random.randint(0,420))
+        y = 0
+        vel = 2
+        run = True
+        
+        
+                
+        while run:
+            reloj = pygame.time.Clock()
+            y += vel#Incrementa a "y" en cada vuelta
+            pantalla.fill((0,0,0))#PantallaNegra
             
-            
-    while run:
-        reloj = pygame.time.Clock()
-        y += vel#Incrementa a "y" en cada vuelta
-        pantalla.fill((0,0,0))#PantallaNegra
 
-        reloj.tick(20)
+            reloj.tick(20)
 
-        pantalla.blit(texto1, (x,y))#Muestra el texto en las posiciones x e y
+            for e in pygame.event.get():
+                if e.type == KEYDOWN:
+                    letra = dameLetraApretada(e.key)
+                    palabraUsuario += letra
 
-        pygame.draw.line(pantalla, (40, 210, 250), (0, 500), (400, 500), 1)#Linea = Pantalla, color, start_pos, end_pos, Ancho
-        pygame.display.update()#Actualiza la pantalla
-        if y == 500:
-            vel = 0
+            pantalla.blit(texto, (x,y))#Muestra el texto en las posiciones x e y
+            pantalla.blit(puntaje,(300,12))#Palabra puntaje esquina superior
+            pygame.draw.line(pantalla, (40, 210, 250), (0, 500), (400, 500), 1)#Linea = Pantalla, color, start_pos, end_pos, Ancho
+            pantalla.blit(fuente.render(palabraUsuario,1, (255,0,0)),(0,0))
+            pygame.display.update()#Actualiza la pantalla
+            if palabraUsuario == palabra:
+                puntos += 10
+                vel = 0
+                palabraUsuario = ""
+                break
 
-    pygame.quit()
+            if y == 500:
+                vel = 0
+                puntos -=10
+                break
+
+   
+   
 
 
 
 if __name__ == '__main__':
   
-    main()
+    #main()
+    
+
+    dicc = {
+        92: "a",
+        93: "b",
+        94:"c",
+        95: ""
+
+    }
+
+    dicc[92]
